6c0a1eb20d9b5145a4686210272f3d4a91740403
AndreeaIonita <ionitaandy@gmail.com>
2025-04-02 15:31:21 +0300

    functionalitate - notificare atunci cand se adauga o carte

--- BookTrack/app/src/main/java/com/example/booktrack/ui/my_books/add_book/AddBookFragment.kt
+++ BookTrack/app/src/main/java/com/example/booktrack/ui/my_books/add_book/AddBookFragment.kt
@@ -1,5 +1,6 @@
 package com.example.booktrack.ui.my_books.add_book
 
+import NotificationViewModel
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
@@ -11,12 +12,16 @@ import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.booktrack.R
 import com.example.booktrack.data.database.AppDatabase
 import com.example.booktrack.data.models.Book
+import com.example.booktrack.data.models.Notification
 import com.example.booktrack.data.repositories.BookRepository
+import com.example.booktrack.data.repositories.NotificationRepository
+import com.example.booktrack.data.viewModels.NotificationViewModelFactory
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -28,6 +33,10 @@ class AddBookFragment : Fragment(R.layout.fragment_add_book) {
     private lateinit var editTextDescription: EditText
     private lateinit var spinnerGenre: Spinner
     private lateinit var editTextAuthor: EditText
+
+    // pentru notif
+    private lateinit var notificationViewModel: NotificationViewModel
+
 
     private var coverUri: Uri? = null
     private var selectedCoverResId: Int = R.drawable.hunger_games // seteaza o coperta default
@@ -49,6 +58,13 @@ class AddBookFragment : Fragment(R.layout.fragment_add_book) {
         editTextDescription = view.findViewById(R.id.editTextDescription)
         spinnerGenre = view.findViewById(R.id.spinnerGenre)
         editTextAuthor = view.findViewById(R.id.editTextAuthor)
+
+        // notif
+        val dao = AppDatabase.getDatabase(requireContext()).notificationDao()
+        val repo = NotificationRepository(dao)
+        val factory = NotificationViewModelFactory(requireActivity().application, repo)
+        notificationViewModel = ViewModelProvider(this, factory)[NotificationViewModel::class.java]
+
 
         spinnerGenre.setSelection(0, false)
 
@@ -97,7 +113,17 @@ class AddBookFragment : Fragment(R.layout.fragment_add_book) {
             coverImageFileName = coverImageFileName // salveaza numele fisierului
         )
 
+
         bookViewModel.insertBook(book)
+
+        lifecycleScope.launch {
+            val notification = Notification(
+                title = "Carte nouă adăugată",
+                message = "La genul $genre s-a adăugat cartea $title de autorul $author"
+            )
+            notificationViewModel.insertNotification(notification)
+        }
+
 
         Toast.makeText(requireContext(), "The book has been added!", Toast.LENGTH_SHORT).show()
         findNavController().popBackStack(R.id.navigation_my_books, false)
